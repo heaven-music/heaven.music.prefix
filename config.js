327be671bcff2a860ab24e4013cb29db1127ce64
Anshuman-hub17 <anshumansahubam1705@gmail.com>
2025-07-31 08:35:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["894898932549832714", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
