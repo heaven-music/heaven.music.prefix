7c03796210226cbbe7b8eba10e18b3ac59ba3a1c
heaven-music <heaven.musicbot@gmail.com>
2025-08-08 12:01:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,28 @@
-
-
 module.exports = {
   TOKEN: "",
+  prefix: ".", // ⬅️ NEW: Prefix for normal commands
   language: "en",
-  ownerID: ["894898932549832714", ""], 
-  mongodbUri : "mongodb+srv://heaven:heaven@discordbot.hxbqucv.mongodb.net/?retryWrites=true&w=majority",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["894898932549832714"], // Your ID (remove empty string)
+  
+  mongodbUri: "mongodb+srv://heaven:heaven@discordbot.hxbqucv.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId: "",
+  spotifyClientSecret: "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "LISTENING", // Available activity types: LISTENING , PLAYING
   SupportServer: "https://discord.gg/h4heaven",
   embedTimeout: 5, 
   errorLog: "", 
+
   nodes: [
-     {
+    {
       name: "GlaceYT",
       password: "glaceyt",
       host: "5.39.63.207",
-      port:  8262,
+      port: 8262,
       secure: false
     }
   ]
-}
+};
