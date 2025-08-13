33a9cddffbda9e86203f6e324a530973d2fca81f
heaven-music <heaven.musicbot@gmail.com>
2025-08-13 22:20:37 +0530

    Add files via upload

---
+++ prefixCommands/support.js
@@ -0,0 +1,49 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+const musicIcons = require('../UI/icons/musicicons.js');
+
+module.exports = {
+    name: "support",
+    description: "Get support server link",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction, lang) => {
+        try {
+            const supportServerLink = "https://discord.gg/h4heaven";
+            const githubLink = "https://github.com";
+            const replitLink = "https://replit.com";
+            const youtubeLink = "https://www.youtube.com";
+
+            const embed = new EmbedBuilder()
+                .setColor('#B5E6EF')
+                .setAuthor({
+                    name: lang.support.embed.authorName,
+                    iconURL: musicIcons.beats2Icon, 
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.description
+                    .replace("{supportServerLink}", supportServerLink)
+                    .replace("{githubLink}", githubLink)
+                    .replace("{replitLink}", replitLink)
+                    .replace("{youtubeLink}", youtubeLink)
+                )
+                .setImage('https://cdn.discordapp.com/attachments/1400318343725973544/1401246478197981184/Hangout_heaven.png?ex=688f93fa&is=688e427a&hm=d5f813ab3d05b6a4a46ea5ce4357d19754af177de4d15acbd5c3bf28de749555&')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#ff0000')
+                .setAuthor({
+                    name: lang.support.embed.error,
+                    iconURL: musicIcons.alertIcon,
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.errorDescription)
+                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};
